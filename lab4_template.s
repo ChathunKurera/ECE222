e20a380d4c5b213f0cbf2d4b858d36f36b200246
ychathun <ychathun@gmail.com>
2016-01-17 02:36:43 -0500

    all files added

---
+++ lab4_template.s
@@ -0,0 +1,104 @@
+;*-------------------------------------------------------------------
+;* Name:    	lab_4_program.s 
+;* Purpose: 	A sample style for lab-4
+;* Term:		Winter 2014
+;*-------------------------------------------------------------------
+				THUMB 								; Declare THUMB instruction set 
+				AREA 	My_code, CODE, READONLY 	; 
+				EXPORT 		__MAIN 					; Label __MAIN is used externally 
+				EXPORT 		EINT3_IRQHandler 	; without this the interupt routine will not be found
+
+				ENTRY 
+
+__MAIN
+
+; The following lines are similar to previous labs.
+; They just turn off all LEDs 
+				LDR			R10, =LED_BASE_ADR		; R10 is a  pointer to the base address for the LEDs
+				MOV 		R3, #0xB0000000		; Turn off three LEDs on port 1  
+				STR 		R3, [r10, #0x20]
+				MOV 		R3, #0x0000007C
+				STR 		R3, [R10, #0x40] 	; Turn off five LEDs on port 2 
+
+; This line is very important in your main program
+; Initializes R11 to a 16-bit non-zero value and NOTHING else can write to R11 !!
+				MOV			R11, #0xABCD		; Init the random number generator with a non-zero number
+LOOP 			BL 			RNG  
+				B 			LOOP
+
+				
+				
+		;
+		; Your main program can appear here 
+		;
+				
+				
+				
+;*------------------------------------------------------------------- 
+; Subroutine RNG ... Generates a pseudo-Random Number in R11 
+;*------------------------------------------------------------------- 
+; R11 holds a random number as per the Linear feedback shift register (Fibonacci) on WikiPedia
+; R11 MUST be initialized to a non-zero 16-bit value at the start of the program
+; R11 can be read anywhere in the code but must only be written to by this subroutine
+RNG 			STMFD		R13!,{R1-R3, R14} 	; Random Number Generator 
+				AND			R1, R11, #0x8000
+				AND			R2, R11, #0x2000
+				LSL			R2, #2
+				EOR			R3, R1, R2
+				AND			R1, R11, #0x1000
+				LSL			R1, #3
+				EOR			R3, R3, R1
+				AND			R1, R11, #0x0400
+				LSL			R1, #5
+				EOR			R3, R3, R1			; The new bit to go into the LSB is present
+				LSR			R3, #15
+				LSL			R11, #1
+				ORR			R11, R11, R3
+				LDMFD		R13!,{R1-R3, R15}
+
+;*------------------------------------------------------------------- 
+; Subroutine DELAY ... Causes a delay of 1ms * R0 times
+;*------------------------------------------------------------------- 
+; 		aim for better than 10% accuracy
+DELAY			STMFD		R13!,{R2, R14}
+		;
+		; Code to generate a delay of 1mS * R0 times
+		;
+exitDelay		LDMFD		R13!,{R2, R15}
+
+; The Interrupt Service Routine MUST be in the startup file for simulation 
+;   to work correctly.  Add it where there is the label "EINT3_IRQHandler
+;
+;*------------------------------------------------------------------- 
+; Interrupt Service Routine (ISR) for EINT3_IRQHandler 
+;*------------------------------------------------------------------- 
+; This ISR handles the interrupt triggered when the INT0 push-button is pressed 
+; with the assumption that the interrupt activation is done in the main program
+EINT3_IRQHandler 	
+					STMFD 		... 				; Use this command if you need it  
+		;
+		; Code that handles the interrupt 
+		;
+					LDMFD 		... 				; Use this command if you used STMFD (otherwise use BX LR) 
+
+
+;*-------------------------------------------------------------------
+; Below is a list of useful registers with their respective memory addresses.
+;*------------------------------------------------------------------- 
+LED_BASE_ADR	EQU 	0x2009c000 		; Base address of the memory that controls the LEDs 
+PINSEL3			EQU 	0x4002C00C 		; Pin Select Register 3 for P1[31:16]
+PINSEL4			EQU 	0x4002C010 		; Pin Select Register 4 for P2[15:0]
+FIO1DIR			EQU		0x2009C020 		; Fast Input Output Direction Register for Port 1 
+FIO2DIR			EQU		0x2009C040 		; Fast Input Output Direction Register for Port 2 
+FIO1SET			EQU		0x2009C038 		; Fast Input Output Set Register for Port 1 
+FIO2SET			EQU		0x2009C058 		; Fast Input Output Set Register for Port 2 
+FIO1CLR			EQU		0x2009C03C 		; Fast Input Output Clear Register for Port 1 
+FIO2CLR			EQU		0x2009C05C 		; Fast Input Output Clear Register for Port 2 
+IO2IntEnf		EQU		0x400280B4		; GPIO Interrupt Enable for port 2 Falling Edge 
+ISER0			EQU		0xE000E100		; Interrupt Set-Enable Register 0 
+ISER0			EQU		0xE000E100		; Interrupt Set-Enable Register 0 
+IO2INTCLR			EQU		0x....		; Interrupt Port 2 Clear Register
+
+				ALIGN 
+
+				END 
